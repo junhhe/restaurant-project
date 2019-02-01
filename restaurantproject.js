3b38e15034728328459610763d23710ad222eb27
junhhe <42394094+junhhe@users.noreply.github.com>
2019-01-31 22:22:25 -1000

    Add files via upload

--- restaurantproject.js
+++ restaurantproject.js
@@ -5,6 +5,9 @@ var don;
 var siumai;
 var jian;
 
+ 
+    
+
 var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function(){
       if  (this.readyState == 4 && this.status == 200){   // I will do this when requirements are met
@@ -28,58 +31,77 @@ var xmlhttp = new XMLHttpRequest();
     var jian = mydata.food[5].price;
     var jianName = mydata.food[5].name;
       
-    var one = Number(localStorage.setItem(hargow));
-    var two = Number(localStorage.setItem(bao));
-    var three = Number(localStorage.setItem(feng));
-    var four = Number(localStorage.setItem(don));
-    var five = Number(localStorage.setItem(siumai));
-    var six = Number(localStorage.setItem(jian));
-    
+   
   document.getElementById("sum1").innerHTML =  hargowName + "<br>" + "$" + hargow + "<br>" + mydata.food[0].description;
  document.getElementById("sum2").innerHTML = baoName + "<br>" + "$" + bao + "<br>" + mydata.food[1].description;
  document.getElementById("sum3").innerHTML = fengName + "<br>" + "$" + feng + "<br>" + mydata.food[2].description;
  document.getElementById("sum4").innerHTML = donName + "<br>" + "$" + don + "<br>" + mydata.food[3].description;
  document.getElementById("sum5").innerHTML = siumaiName + "<br>" + "$" + siumai + "<br>" + mydata.food[4].description;
  document.getElementById("sum6").innerHTML = jianName + "<br>" + "$" + jian + "<br>" + mydata.food[5].description;
-      }
+ 
+  var one = Number(localStorage.setItem(hargow));
+  var uno = localStorage.setItem(hargowName);
+    var two = Number(localStorage.setItem(bao));
+    var dos = localStorage.setItem(baoName);
+    var three = Number(localStorage.setItem(feng));
+    var tres = localStorage.setItem(fengName);
+    var four = Number(localStorage.setItem(don));
+    var cuatro = localStorage.setItem(donName);
+    var five = Number(localStorage.setItem(siumai));
+    var cinco = localStorage.setItem(siumaiName);
+    var six = Number(localStorage.setItem(jian));
+    var seis = localStorage.setItem(jianName);
+  }
     };
+
       
  function dimsum1(){
+   var name1 = Number(localStorage.getItem(hargow));
+   window.alert(name1); //testing
    var qty = document.getElementById('quantity1').value;
- var total1 = ((qty * 1.04712)*5).toFixed(2);
+ var total1 = ((qty * 5)*1.04712).toFixed(2);
  document.getElementById("buy1").innerHTML = "You total is" + " $" + total1;
-      }
+ var price1 = Number(localStorage.setItem("firsttotal",total1));
+
+}
   
   function dimsum2(){
    var qty = document.getElementById('quantity2').value;
- var total1 = ((qty * 1.04712)*3).toFixed(2);
- document.getElementById("buy2").innerHTML = "You total is" + " $" + total1;
-      }
+ var total2 = ((qty * 1.04712)*3).toFixed(2);
+ document.getElementById("buy2").innerHTML = "You total is" + " $" + total2;
+ var price2= Number(localStorage.setItem(total2));
+}
       
   function dimsum3(){
    var qty = document.getElementById('quantity3').value;
- var total1 = ((qty * 1.04712)*4).toFixed(2);
- document.getElementById("buy3").innerHTML = "You total is" + " $" + total1;
-      }
+ var total3 = ((qty * 1.04712)*4).toFixed(2);
+ document.getElementById("buy3").innerHTML = "You total is" + " $" + total3;
+ var price3 = Number(localStorage.setItem(total3));
+}
       
 function dimsum4(){
    var qty = document.getElementById('quantity4').value;
- var total1 = ((qty * 1.04712)*1.5).toFixed(2);
- document.getElementById("buy4").innerHTML = "You total is" + " $" + total1;
-      }
+ var total4 = ((qty * 1.04712)*1.5).toFixed(2);
+ document.getElementById("buy4").innerHTML = "You total is" + " $" + total4;
+ var price4 = Number(localStorage.setItem(total4));
+}
       
     function dimsum5(){
    var qty = document.getElementById('quantity5').value;
- var total1 = ((qty * 1.04712)*3.5).toFixed(2);
- document.getElementById("buy5").innerHTML = "You total is" + " $" + total1;
-      }
+ var total5 = ((qty * 1.04712)*3.5).toFixed(2);
+ document.getElementById("buy5").innerHTML = "You total is" + " $" + total5;
+ var price5 = Number(localStorage.setItem(total5));
+}
       
       function dimsum6(){
    var qty = document.getElementById('quantity6').value;
- var total1 = ((qty * 1.04712)*1).toFixed(2);
- document.getElementById("buy6").innerHTML = "You total is" + " $" + total1;
-      }
-        
+ var total6 = ((qty * 1.04712)*1).toFixed(2);
+ document.getElementById("buy6").innerHTML = "You total is" + " $" + total6;
+ var price6 = Number(localStorage.setItem(total6));
+}
+   
+      
+    
     
     xmlhttp.open("GET","restaurant.json",true);
     xmlhttp.send();
